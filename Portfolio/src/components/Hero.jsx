aefff0206342be7dcb9db0b243c79dd6b77a218b
Daniyal1229 <syeddaniyalbokhari72@gmail.com>
2023-06-27 10:49:01 +0530

    updates

--- Portfolio/src/components/Hero.jsx
+++ Portfolio/src/components/Hero.jsx
@@ -16,11 +16,10 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Daniyal Bokhari</span>
+            Hi, I'm <span className='text-[#915EFF]'>Daniyal </span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-          Full-Stack Developer with a passion for data analysis, I develop user <br className='sm:block hidden' />
-            interfaces and web applications
+          Passionate full-stack developer specializing in user interfaces, web applications, and data analysis.
           </p>
         </div>
       </div>
